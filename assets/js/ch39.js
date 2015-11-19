182d733efbec20e6f0edbe02417baeb3d0098fc2
Fan Zhang <tonyjohnvan@gmail.com>
2015-11-19 00:10:52 -0500

    some minor fixes

--- assets/js/ch39.js
+++ assets/js/ch39.js
@@ -5,14 +5,14 @@
 var jsonPath = 'data/gv39/data.json';
 var dataToUse;
 
-var aj = $.getJSON(jsonPath)
-        .done(function (json) {
-            prepareChartWithData(json)
-        })
-        .fail(function (error) {
-            console.log(error);
-        })
-    ;
+$.getJSON(jsonPath)
+    .done(function (json) {
+        prepareChartWithData(json)
+    })
+    .fail(function (error) {
+        console.log(error);
+    })
+;
 
 $(window).load(function () {
 });
@@ -31,7 +31,7 @@ function prepareChartWithData(json) {
             '<h1 class="row-label" contenteditable="true">' +
             dataToUse.rows[i].title +
             '</h1>' +
-            '<div class="color-meter" id="'+dataToUse.rows[i].id+'Bar"></div>' +
+            '<div class="color-meter" id="' + dataToUse.rows[i].id + 'Bar"></div>' +
             '<ul class="data-points-wrap" id="d' + dataToUse.rows[i].id + '">' +
             '</ul>' +
             '</li>'
@@ -55,14 +55,41 @@ function prepareChartWithData(json) {
 
         // rearrange the gradient bar
         var leftMost = dataToUse.rows[i].point[0].value * 3.5 + 436;
-        var lastId = dataToUse.rows[i].point.length-1;
+        var lastId = dataToUse.rows[i].point.length - 1;
         var rightMost = dataToUse.rows[i].point[lastId].value * 3.5 + 436 - lastId * 30;
-        $('#'+dataToUse.rows[i].id+'Bar').css({
-            "margin-left":leftMost+45,
-            "width":rightMost-leftMost+lastId*30
+        $('#' + dataToUse.rows[i].id + 'Bar').css({
+            "margin-left": leftMost + 45,
+            "width": rightMost - leftMost + lastId * 30
         });
     }
 
     // rearrange the vertical zero line
     $('.vertical-zero').height(( 110 + 15 * 2 ) * dataToUse.rows.length);
+}
+
+function changeData(num) {
+    switch (num) {
+        case 1 :
+        {
+            jsonPath = 'data/gv39/data.json';
+        }
+            break;
+        case 2 :
+        {
+            jsonPath = 'data/gv39/data2.json';
+        }
+            break;
+        default :
+        {
+            jsonPath = 'data/gv39/data2.json';
+        }
+    }
+    $.getJSON(jsonPath)
+        .done(function (json) {
+            prepareChartWithData(json)
+        })
+        .fail(function (error) {
+            console.log(error);
+        })
+    ;
 }
