c912a8c1960f628e9caa235f83d67b0a261ff221
Fan Zhang <tonyjohnvan@gmail.com>
2016-02-02 15:16:27 -0500

    added realtime data usage for ch 20

--- assets/js/ch20.js
+++ assets/js/ch20.js
@@ -1,28 +1,12 @@
-/**
- * Created by fanzhang on 1/13/16.
- */
-
-var jsonPath;
-var dataToUse;
-
-var fixHelper = function (e, ui) {
-    ui.children().each(function () {
-        $(this).width($(this).width());
-    });
-    return ui;
-};
-
-$(window).load(function () {
-    loadData(1);
+$(function () {
+    convertedServerData('data/gv20.json');
 });
 
-function loadData() {
-    jsonPath = 'data/gv16.json';
+function convertedServerData(jsonPath) {
     $.getJSON(jsonPath)
         .done(function (json) {
-            prepareTableWithData(json, function () {
-                addOtherFeatrures();
-            })
+            console.log(json);
+            initHighChartWith(json);
         })
         .fail(function (error) {
             console.log(error);
@@ -30,50 +14,70 @@ function loadData() {
     ;
 }
 
-function prepareTableWithData(json, callback) {
+function initHighChartWith(data) {
 
-    dataToUse = json.data;
-    var dataWrapper = $(".data-table");
-    dataWrapper.html('');
-    dataWrapper.append('<thead class="tablehead"></thead>');
-    $('.tablehead').append("<tr class='header-tr'></tr>");
-    var tableHeader = $(".header-tr");
-    for (var i = 0; i < dataToUse.series[0].data.length; i++) {
-        if (i >= 1) {
-            tableHeader.append("<th class='text-center'>" + dataToUse.series[0].data[i].x + "</th>");
-        } else {
-            tableHeader.append("<th>" + dataToUse.series[0].data[i].x + "</th>");
-        }
+    var seriesdata = [];
+    var categories = [];
 
-    }
-    //add rows:
-    dataWrapper.append('<tbody class="tableBody"></tbody>');
-    var bodyWrapper = $('.tableBody');
-
-    for (var i = 0; i < dataToUse.series.length; i++) {
-        bodyWrapper.append("<tr id='dataR" + (i + 1) + "'></tr>");
-        var targetRow = $("#dataR" + (i + 1));
-        targetRow.append('<td class="dataCell firstCol">' + (dataToUse.series[i].data[0].y) + '</td>');
-        for (var j = 1; j < dataToUse.series[i].data.length; j++) {
-            if (j === 1) {
-                targetRow.append('<td class="dataCell"><div class="row"><div class="col-xs-11 text-left"><span class="gbar" style="width: ' +
-                    (dataToUse.series[i].data[j].y) + '"></span></div></div></td>');
-            } else {
-                targetRow.append('<td class="dataCell short"><div class="row"><div class="col-xs-12">' + (dataToUse.series[i].data[j].y) + '</div></div></td>');
+    for (var i in data.data.series) {
+        var temp = {};
+        temp.name = data.data.series[i].name;
+        temp.data = [];
+        for (var j in data.data.series[i].data) {
+            temp.data.push(data.data.series[i].data[j].y);
+            if(categories.length<data.data.series[i].data.length){
+                categories.push(data.data.series[i].data[j].x);
             }
         }
+        seriesdata.push(temp);
     }
 
-    // call back function
-    if (callback && typeof(callback) === "function") {
-        callback();
-    }
-}
-
-function addOtherFeatrures() {
-    // JQ sortable
-    $('.tableBody').sortable({
-        helper: fixHelper
-    }).disableSelection();
-    $('.data-table').tablesorter()
+    $('#container').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Driver Chart(Negative)'
+        },
+        subtitle: {
+            text: 'Chart Title (n=215)'
+        },
+        xAxis: {
+            categories: categories,
+            crosshair: false
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Rainfall (mm)',
+                enabled: false
+            }
+        },
+        tooltip: {
+            enabled: false
+        },
+        plotOptions: {
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0,
+                dataLabels: {
+                    enabled: true,
+                    format: "{y}%"
+                }
+            }
+        },
+        series: seriesdata
+        //    [{
+        //    name: 'Brand 1',
+        //    data: [49, 71, 96, 100, 44]
+        //
+        //}, {
+        //    name: 'Brand 2',
+        //    data: [83, 78, 98, 93, 100]
+        //
+        //}, {
+        //    name: 'Brand 3',
+        //    data: [48, 38, 39, 41, 47]
+        //}]
+    });
 }
